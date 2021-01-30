9934b5dd31c5f03e667ad2e57156c478b4285fdf
Matthew Chaplain <KazDragon@users.noreply.github.com>
2021-01-30 11:41:06 +0100

    Build an initial skeleton for the library (#1)
    * Added an initial skeleton for the library.
    * Added Github Workflow actions for CI.

---
+++ .ci/fetch_system_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z ${EXTERNAL_ROOT+x} ]; then echo EXTERNAL_ROOT not set; exit 1; fi
+
+export EXTERNAL_BUILD_ROOT=$HOME/external_build
+
+mkdir "$EXTERNAL_BUILD_ROOT" || true
+
+if [ ! -f "$EXTERNAL_ROOT/include/gtest/gtest.h" ]; then
+    cd "$EXTERNAL_BUILD_ROOT";
+    wget https://github.com/google/googletest/archive/release-1.10.0.tar.gz;
+    tar -xzf release-1.10.0.tar.gz;
+    cd googletest-release-1.10.0;
+    cmake -DCMAKE_INSTALL_PREFIX="$EXTERNAL_ROOT" .;
+    make -j2 && make install;
+fi
---
+++ src/dummy.cpp
@@ -0,0 +1,1 @@
+#include "consolepp/core.hpp"
