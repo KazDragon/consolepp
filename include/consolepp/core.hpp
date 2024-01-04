e254a43d43b94b6d8265b10dda0cc917f1313e85
Matthew Chaplain <matthew.chaplain@vitestro.com>
2024-01-05 00:09:55 +0100

    Implemented automatic code styling, applied modernizations

--- include/consolepp/core.hpp
+++ include/consolepp/core.hpp
@@ -1,5 +1,5 @@
-#include "consolepp/version.hpp"
-#include "consolepp/detail/export.hpp"
+#include "consolepp/detail/export.hpp"  // IWYU pragma: export
+#include "consolepp/version.hpp"        // IWYU pragma: export
 #include <gsl/gsl-lite.hpp>
 #include <cstring>
 
@@ -8,7 +8,7 @@ namespace consolepp {
 // A byte in Console++ is represented consistently as an unsigned 8-bit integer.
 using byte = std::uint8_t;
 
-// A stream of bytes in Console++ is exposed as a non-owning span.  It is 
+// A stream of bytes in Console++ is exposed as a non-owning span.  It is
 // expected that the data will live for no longer than any function in which
 // it is found.  For that reason, these spans should never be stored directly.
 // If storage is necessary, it must be converted into a longer-living data
@@ -22,19 +22,19 @@ using byte_storage = std::basic_string<byte>;
 namespace literals {
 
 // A simple function to convert from string literals to stored bytes.
-inline byte_storage operator ""_cb(char const *text, size_t length)
+inline byte_storage operator""_cb(char const *text, size_t length)
 {
-    byte_storage result;
-    result.reserve(length);
+  byte_storage result;
+  result.reserve(length);
 
-    for (auto ch : gsl::span{text, length})
-    {
-        result.push_back(static_cast<consolepp::byte>(ch));
-    }
+  for (auto ch : gsl::span{text, length})
+  {
+    result.push_back(static_cast<consolepp::byte>(ch));
+  }
 
-    return result;
+  return result;
 }
 
-}
+}  // namespace literals
 
-}
+}  // namespace consolepp
